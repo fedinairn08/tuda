daf181c5c8d87f70759827bc234220f5ab80ca47
fedinairn08 <fedina_irn08@mail.ru>
2025-06-25 23:08:46 +0300

    Проект создан

---
+++ src/main/java/com/tuda/TudaApplication.java
@@ -0,0 +1,13 @@
+package com.tuda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TudaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TudaApplication.class, args);
+	}
+
+}
