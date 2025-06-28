8f4f2df2a308c371415f80162a1b7df7fb854a37
fedinairn08 <fedina_irn08@mail.ru>
2025-06-28 11:32:31 +0300

    Написаны еще ручки для AccountingAppUser

--- src/main/java/com/tuda/repository/AccountingUserRepository.java
+++ src/main/java/com/tuda/repository/AccountingUserRepository.java
@@ -1,8 +1,13 @@
 package com.tuda.repository;
 
 import com.tuda.entity.AccountingAppUser;
+import com.tuda.entity.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AccountingUserRepository extends JpaRepository<AccountingAppUser, Long> {
+import java.util.Optional;
 
+public interface AccountingUserRepository extends JpaRepository<AccountingAppUser, Long> {
+    Optional<AccountingAppUser> findByAppUserAndEvent_Id(AppUser user, Long eventId);
+
+    void deleteByAppUserAndEvent_Id(AppUser user, Long eventId);
 }
