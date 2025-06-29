e666db5eefd29d36e2d57c01d82d609881224e83
fedinairn08 <fedina_irn08@mail.ru>
2025-06-29 18:57:00 +0300

    Доделан один эндпоинт, доделана работа с хранилищем S3

--- src/main/java/com/tuda/service/impl/EventServiceImpl.java
+++ src/main/java/com/tuda/service/impl/EventServiceImpl.java
@@ -2,20 +2,22 @@ package com.tuda.service.impl;
 
 import com.tuda.dto.request.EventRequestDTO;
 import com.tuda.entity.Event;
+import com.tuda.entity.Photo;
 import com.tuda.enums.EventStatus;
 import com.tuda.exception.NotFoundException;
 import com.tuda.repository.EventRepository;
-import com.tuda.repository.RequestRepository;
+import com.tuda.repository.PhotoRepository;
 import com.tuda.repository.UserRepository;
 import com.tuda.service.EventService;
 import com.tuda.service.RequestService;
+import com.tuda.service.file.FileService;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.function.Consumer;
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
@@ -24,6 +26,8 @@ public class EventServiceImpl implements EventService {
     private final EventRepository eventRepository;
     private final UserRepository userRepository;
     private final ModelMapper modelMapper;
+    private final PhotoRepository photoRepository;
+    private final FileService fileService;
 
     @Override
     @Transactional(readOnly = true)
@@ -60,7 +64,26 @@ public class EventServiceImpl implements EventService {
 
         modelMapper.map(requestDTO,event);
 
+        if (requestDTO.getFilename() != null && requestDTO.getUuid() != null && Objects.isNull(event.getPhoto())) {
+            Photo photo = new Photo(requestDTO.getUuid(), requestDTO.getFilename());
+            photoRepository.save(photo);
+            event.setPhoto(photo);
+        } else if (requestDTO.getFilename() == null && requestDTO.getUuid() == null
+                && Objects.nonNull(event.getPhoto())) {
+            fileService.delete(event.getPhoto().getFilename());
+            photoRepository.delete(event.getPhoto());
+            event.setPhoto(null);
+        } else if (requestDTO.getFilename() != null && requestDTO.getUuid() != null) {
+            fileService.delete(event.getPhoto().getFilename());
 
+            Photo photo = photoRepository.findById(event.getPhoto().getId())
+                    .orElseThrow(() -> new NotFoundException("Photo not found with id: " + event.getPhoto().getId()));
+
+            photo.setFilename(requestDTO.getFilename()).setUploadId(requestDTO.getUuid());
+            photoRepository.save(photo);
+
+            event.setPhoto(photo);
+        }
         return eventRepository.save(event);
     }
 
@@ -72,8 +95,12 @@ public class EventServiceImpl implements EventService {
 
         event.setEventStatus(EventStatus.WILL);
 
+        if (requestDTO.getFilename() != null && requestDTO.getUuid() != null) {
+            Photo photo = new Photo(requestDTO.getUuid(), requestDTO.getFilename());
+            photoRepository.save(photo);
+            event.setPhoto(photo);
+        }
+
         return eventRepository.save(event);
     }
-
-
 }
