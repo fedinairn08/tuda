1092198439476788a0f007b50352df6faac16b9c
fedinairn08 <fedina_irn08@mail.ru>
2025-07-07 23:05:32 +0300

    Отключен cors, изменена версия minio

--- src/main/java/com/tuda/config/ApplicationConfiguration.java
+++ src/main/java/com/tuda/config/ApplicationConfiguration.java
@@ -5,14 +5,21 @@ import io.micrometer.core.instrument.MeterRegistry;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @RequiredArgsConstructor
-public class ApplicationConfiguration {
+public class ApplicationConfiguration implements WebMvcConfigurer {
     @Bean
     public Counter eventCounter(MeterRegistry prometheusMeterRegistry) {
         return Counter.builder("event.created")
             .description("Number of created events")
             .register(prometheusMeterRegistry);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
     }
 }
