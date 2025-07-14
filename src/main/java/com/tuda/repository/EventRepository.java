7de2ff22729b423e369418ec4a1f5a2d9812020c
Konenkov Daniel <d4nil.987@yandex.ru>
2025-07-14 19:56:11 +0300

    Учтены гости при выводе меропритятий, где необзрдимы участники и волонтеры

--- src/main/java/com/tuda/repository/EventRepository.java
+++ src/main/java/com/tuda/repository/EventRepository.java
@@ -19,20 +19,8 @@ public interface EventRepository extends JpaRepository<Event, Long> {
     @Query(value = "SELECT e.* FROM events e JOIN organizations o ON e.organization_id = o.id JOIN app_users ap ON o.id = ap.organization_id WHERE ap.id = :organizerId", nativeQuery = true)
     List<Event> findAllOrganizationEventsByOrganizerId(@Param("organizerId") long organizerId);
 
-    @Query(value = "SELECT e.* FROM events e LEFT JOIN (SELECT ac.event_id, COUNT(ac.id) as participant_count FROM accounting_app_users ac WHERE ac.user_role = :userRole GROUP BY ac.event_id) acc ON e.id = acc.event_id WHERE (e.participants_number > acc.participant_count OR acc.participant_count is NULL) AND e.event_status = 'WILL'", nativeQuery = true)
-    List<Event> findAllByNeededParticipantForUser(@Param("userRole") long userRole);
-
-    @Query(value = "SELECT e.* FROM events e LEFT JOIN (SELECT ac.event_id, COUNT(ac.id) as volunteer_count FROM accounting_app_users ac WHERE ac.user_role = :userRole GROUP BY ac.event_id) acc ON e.id = acc.event_id WHERE (e.volunteers_number > acc.volunteer_count OR acc.volunteer_count is NULL) AND e.event_status = 'WILL'", nativeQuery = true)
-    List<Event> findAllByNeededVolunteersForUser(@Param("userRole") long userRole);
-
     @Query(value = "SELECT e.* FROM events e JOIN organizations o ON e.organization_id = o.id JOIN app_users app ON o.id = app.organization_id WHERE app.id = :appUserId AND e.event_status = :status", nativeQuery = true)
     List<Event> findAllByStatusAndAppUserIdForOrganizer(@Param("appUserId") long appUserId, @Param("status") String status);
-
-    @Query(value = "SELECT e.* FROM events e JOIN organizations o ON e.organization_id = o.id JOIN app_users app ON o.id = app.organization_id WHERE app.id = :appUserId INTERSECT SELECT e.* FROM events e LEFT JOIN (SELECT ac.event_id, COUNT(ac.id) as participant_count FROM accounting_app_users ac WHERE ac.user_role = :userRole GROUP BY ac.event_id) acc ON e.id = acc.event_id WHERE e.participants_number > acc.participant_count OR acc.participant_count is NULL", nativeQuery = true)
-    List<Event> findAllByNeededParticipantForOrganizer(@Param("appUserId") long appUserId, @Param("userRole") long userRole);
-
-    @Query(value = "SELECT e.* FROM events e JOIN organizations o ON e.organization_id = o.id JOIN app_users app ON o.id = app.organization_id WHERE app.id = :appUserId INTERSECT SELECT e.* FROM events e LEFT JOIN (SELECT ac.event_id, COUNT(ac.id) as volunteer_count FROM accounting_app_users ac WHERE ac.user_role = :userRole GROUP BY ac.event_id) acc ON e.id = acc.event_id WHERE e.volunteers_number > acc.volunteer_count OR acc.volunteer_count is NULL", nativeQuery = true)
-    List<Event> findAllByNeededVolunteersForOrganizer(@Param("appUserId") long appUserId, @Param("userRole") long userRole);
 
     @Query(value = "SELECT COUNT(acc.id) as user_count FROM accounting_app_users acc WHERE acc.user_role = :userRole AND acc.event_id = :eventId GROUP BY acc.event_id\n", nativeQuery = true)
     Optional<Long> findUserCountByCertainRoleAndEventId(@Param("userRole") long userRole, @Param("eventId")  long eventId);
